d963a9c89681442b8e31dc15dbd994d434b6b9b9
NickSolante <41457938+NickSolante@users.noreply.github.com>
2020-01-16 17:12:04 +1100

    figuring out how to create now lines dynamically

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -8,5 +8,45 @@ export const AddTodo = () => {
   const [iType, setItype] = useState('')
   const [qty, setQty] = useState('')
   const [description, setDescription] = useState('')
-  return <div></div>
+  return (
+    <div>
+      <Row>
+        <Col>
+          <Form.Group controlId='formQty'>
+            <Form.Control
+              type='number'
+              value={qty}
+              className='input'
+              onChange={e => setQty(e.target.value)}
+              placeholder='Qty'
+            />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId='formDescription'>
+            <Form.Control
+              type='text'
+              value={description}
+              className='input'
+              onChange={e => setDescription(e.target.value)}
+              placeholder='Description'
+            />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId='formName'>
+            <Form.Control
+              type='text'
+              className='input'
+              value={iType}
+              onChange={e => setItype(e.target.value)}
+              placeholder=' Type'
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+    </div>
+  )
 }
+
+export default AddTodo
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,18 +3,24 @@ import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import AddTodo from './AddTodo'
 
 function TodoForm({ addTodo }) {
   const [value, setValue] = useState('')
+  const [lines, setLines] = useState(0)
 
   const handleSubmit = e => {
     e.preventDefault()
     if (!value) return
-    addTodo(value, iType, qty, description)
+    addTodo(value)
     setValue('')
-    setItype('')
-    setQty('')
-    setDescription('')
+  }
+
+  const rowAddition = () => {
+    setLines(prevState => prevState + 1)
+    console.log(lines)
+    let rowItem = new Array()
+    for (let i = 0; i <= lines; i++) {}
   }
 
   return (
@@ -33,48 +39,17 @@ function TodoForm({ addTodo }) {
             </Form.Group>
           </Col>
         </Row>
-
         <Row>
           <Col>
-            <Form.Group controlId='formQty'>
-              <Form.Control
-                type='number'
-                value={qty}
-                className='input'
-                onChange={e => setQty(e.target.value)}
-                placeholder='Qty'
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId='formDescription'>
-              <Form.Control
-                type='text'
-                value={description}
-                className='input'
-                onChange={e => setDescription(e.target.value)}
-                placeholder='Description'
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId='formName'>
-              <Form.Control
-                type='text'
-                className='input'
-                value={iType}
-                onChange={e => setItype(e.target.value)}
-                placeholder=' Type'
-              />
-            </Form.Group>
+            <AddTodo></AddTodo>
           </Col>
         </Row>
 
         <Button variant='primary' type='submit'>
           Submit
         </Button>
-        <Button variant='primary' onClick={}>
-          Submit
+        <Button variant='primary' onClick={rowAddition}>
+          Add item
         </Button>
       </Form>
     </form>
